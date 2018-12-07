93e1c19c27b64b9547c9d615efafa572ec31c6de
Al-Names <al.musty.saeed@gmail.com>
2018-12-07 13:21:29 +0100

    redesign in progress

--- src/components/about.js
+++ src/components/about.js
@@ -1,70 +1,90 @@
-import React, { Component } from 'react';
-import Autism from './autism'
-import Charity from './charity'
+import React, { Component } from "react";
+import Autism from "./autism";
+import Charity from "./charity";
+
+import Slide from "react-reveal/Slide";
 
 const hashtag = {
-    color: '#61dafb'
+  color: "#61dafb"
 };
 
 const spacer = {
-    height: '180px'
+  height: "180px"
 };
 
-
-
 class About extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container-fluid wasder-blurb">
+          <h1>Welcome</h1>
+          <hr />
+          <p>Hello there!</p>
+          <p>
+            As you probably know my name is (Al) Mustpha; Al-M or Musty if you
+            prefer and this is my little website. Do bear with me, there is a
+            lot left to do hidden. For both this and the other half Names. But I
+            am indeed working as fast as I can to complete this awesome project.{" "}
+          </p>
+          <p>Now...</p>
 
-    render() {
-        return (
-            <div>
-            <div className="container-fluid wasder-blurb">
-                <h1>Welcome</h1>
-                <hr />
-                <p>Hello there!</p>
-                <p>As you probably know my name  is (Al) Mustpha; Al-M or Musty if you prefer and this is my little website. Do bear with me, there is a lot left to do hidden. For both this and the other half Names. But I am indeed working as fast as I can to complete this awesome project. </p>
-                <p>Now...</p>
+          <h2>About Me</h2>
 
-                <h2>About Me</h2>
+          <p>
+            I'm a 23-year-old developer with my time split between Abuja,
+            Nigeria and Glasgow, UK. Though I tend to focus a lot on design and
+            aesthetics, I think of myself as a fullstack developer. I'm one of
+            the few who realise that design is just as important as
+            functionality.
+          </p>
 
-                <p>I'm a 23-year-old developer with my time split between Abuja, Nigeria and Glasgow, UK. Though I tend to focus a lot on design and aesthetics, I think of myself as a fullstack developer. I'm one of the few who realise that design is just as important as functionality.</p>
+          <p>
+            My favorite projects to work on usually involve one or two
+            technologies that I don't have that much knowledge of, as I love to
+            learn new things and a challenge. However, my main technologies are{" "}
+            <span style={hashtag}>React, React-Native, Angular</span> and{" "}
+            <span style={hashtag}>Ruby on Rails</span> among others. Though I do
+            love good ol' <span style={hashtag}>HTML, CSS, jQuery</span> and of
+            course plain old <span style={hashtag}>vanilla Javascript</span>
+          </p>
+          <hr />
+          <br />
 
-                <p>My favorite projects to work on usually involve one or two technologies that I don't have that much knowledge of, as I love to learn new things and a challenge. However, my main technologies are <span style={hashtag}>React, React-Native, Angular</span> and <span style={hashtag}>Ruby on Rails</span> among others. Though I do love good ol' <span style={hashtag}>HTML, CSS, jQuery</span> and of course plain old <span style={hashtag}>vanilla Javascript</span></p>
-                <hr />
-                <br />
-
-                <div className="looking">
-                    <p>
-                        Right now, I am currently in the market for an adventure... as well as junior to mid development roles that will let me learn and grow as well as provide opportunities to prove myself. Ideally with a relocation package or remote option thrown in.
-                </p>
-                    <p>
-                        As I am always up for a challenge and learning new things, an ideal company would be one that would push me further while letting me build on what I know and can do now as well as one with the chance to be creative.
-                </p>
-                    <p>
-                        Please, feel free to reach out if you have anything for me... including questions.
-                </p>
-                    <p>
-                        Sincerely,
-                </p>
-                    <h4>Al-M</h4>
-                </div>
-                <hr/>
-
-            </div>
-            <h4>Few things that matter to me... and should to you too.</h4>
-                <div>
-                    <h4>Autism and Employment</h4>
-                    <Autism />
-                </div>
-                <div style={spacer}></div>
-                <div>
-                    <h4>Code, Charity and The Future</h4>
-                    <Charity/>
-                </div>
-            </div>
-        )
-    }
-
+          <div className="looking">
+            <p>
+              Right now, I am currently in the market for an adventure... as
+              well as junior to mid development roles that will let me learn and
+              grow as well as provide opportunities to prove myself. Ideally
+              with a relocation package or remote option thrown in.
+            </p>
+            <p>
+              As I am always up for a challenge and learning new things, an
+              ideal company would be one that would push me further while
+              letting me build on what I know and can do now as well as one with
+              the chance to be creative.
+            </p>
+            <p>
+              Please, feel free to reach out if you have anything for me...
+              including questions.
+            </p>
+            <p>Sincerely,</p>
+            <h4>Al-M</h4>
+          </div>
+          <hr />
+        </div>
+        <h4>Few things that matter to me... and should to you too.</h4>
+        <div>
+          <h4>Autism and Employment</h4>
+          <Autism />
+        </div>
+        <div style={spacer} />
+        <div>
+          <h4>Code, Charity and The Future</h4>
+          <Charity />
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default About;
--- src/components/favorites.js
+++ src/components/favorites.js
@@ -1,42 +1,52 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from "react";
 
 class Favorites extends Component {
+  state = {
+    title: [
+      "KonoSuba Live!",
+      "React Contact Manager",
+      "React Journal",
+      "Palettes",
+      "Onyx Hive"
+    ],
+    detail: [
+      "Fictional KonoSuba Live event and ticket sales lading page!",
+      "Contact manager using react",
+      "A simple journal, diary or notepad built on react on a firebase back-end.",
+      " Who doesnt love an RGB color game built purely on CSS and JavaScript?",
+      "An innovative and beautiful landing page design."
+    ],
+    link: [
+      "https://github.com/Al-Names/KonoSuba-Live",
+      "https://github.com/Al-Names/react-contact-manager",
+      "https://sleepy-bohr-8e5cae.netlify.com",
+      "https://codepen.io/Names/pen/GvBryP",
+      "https://codepen.io/Names/pen/PJZVBx"
+    ],
+    delay: [500, 0, 500]
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="highlights">
-                    <h2 className="text-center">Favorites</h2>
-                    <div className="row tabs">
-                        <div className="col-md-6 article">
-                            <a href="https://sleepy-bohr-8e5cae.netlify.com" target="_blank" className="App-link text-center" ><h3>React Journal</h3></a>
-                            <hr />
-                            <p>A simple journal, diary or notepad built on react on a firebase back-end.</p>
-    
-                        </div>
-                        <div className="col-md-6 article">
-                            <a href="https://codepen.io/Names/pen/PJZVBx" target="_blank" className="App-link text-center"><h3>Onyx Hive</h3></a>
-                            <hr />
-                            <p>An innovative and beautiful landing page design.</p>
-                          
-                        </div>
-                        <div className="col-md-6 article" >
-                            <a href="https://codepen.io/Names/pen/GvBryP" target="_blank" className="App-link text-center"><h3>Palettes</h3></a>
-                            <hr />
-                            <p>Who doesn't love an RGB color game built purely on CSS and JavaScript?</p>
-                      
-                        </div>
+  showFavorites = () =>
+    this.state.title.map((item, i) => (
+      <div className="">
+        <a href={this.state.link[i]} target="_blank" className="App-link ">
+          <h3>{this.state.title[i]}</h3>
+        </a>
 
-                    </div>
-
-                </div>
-            </div>
-        )
-    }
-
+        <p>{this.state.detail[i]}</p>
+        <hr />
+      </div>
+    ));
+  render() {
+    return (
+      <div>
+        <div className="highlights">
+          <h2 className="text-center">Favorites</h2>
+          {this.showFavorites()}
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Favorites;
